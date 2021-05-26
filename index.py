b5fdee899edcb9db678712cceb6973b9f61840fd
Gabriel Löff Sá de Moraes <gabriel.moraes@e-unicred.com.br>
2021-05-26 18:58:55 -0300

    Ajustes finais e comentarios

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import sys
-
+from Crypto.Hash import SHA256
 
 """Divide a lista em pedaços de tamanho n"""
 def generate_chunks(lst, n):
@@ -11,14 +11,29 @@ def generate_chunks(lst, n):
 """Inicio da Execução do Programa """
 if len(sys.argv) < 2:
     print("Utilização: python .\\index.py {File Name}")
-    quit
+    quit()
 
 """Leitura dos bytes do arquivo """
 file = open(sys.argv[1], "rb")
 content = file.read()
 file.close()
 
+""" Chama função para dividir a lista de bytes em tamanhos iguais"""
 chunks = generate_chunks(content, 1024)
 
+last_hash = -1
+""" Itera pela lista de blocos de byets em ordem reversa iniciando pelos ultimos bytes do arquivo """
 for x in reversed(list(chunks)):
-    print(x)
+    """ Verifica se é a primeira iteração caso seja realiza o hash do bloco b(n-1) e salva para proxima iteração"""
+    if last_hash == -1:
+        h = SHA256.new(x)
+        last_hash = h.digest()
+    else:
+        """ Caso não seja a primeira iteração adiciona o bloco de bytes para realizar o hash 
+        e contatena com o hash do bloco anterior """
+        h = SHA256.new(x)
+        h.update(last_hash)
+        last_hash = h.digest()
+
+""" Após toda iteração do script é retornado o valor encontrado para o hash do bloco b(0) informado como h0 """
+print("h0 = " + h.hexdigest())
